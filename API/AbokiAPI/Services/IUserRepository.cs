8a0c35129475d5ed43e0303523fe2a4ec0efaced
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-11-23 17:27:46 -0800

    Refractoring Ongoing

---
+++ API/AbokiAPI/Services/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using AbokiData.Configuration;
+using AbokiData.Models.DTOs.Requests;
+using AbokiData.Models.DTOs.Responses;
+using System.Threading.Tasks;
+
+namespace AbokiAPI.Services
+{
+    public interface IUserRepository
+    {
+        Task<RegistrationResponse> LoginAsync(UserLoginRequest user);
+        Task<AuthResult> RefreshTokenAsync(TokenRequest tokenRequest);
+        Task<RegistrationResponse> RegisterUserAsync(UserRegistrationDto user);
+    }
+}
